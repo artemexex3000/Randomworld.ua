7252849b930e6b57dcfc7f5426827f62f3c30ed6
artemexex3000 <mrtrololoshka70@gmail.com>
2018-07-06 21:49:50 +0400

    RandomWorld ALPHA
    The best site for random numbers was transferred to the best site for flipping
    a coin

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 	<html>
-	<head>
-		<title>
-		Random things
-		</title>
-	</head>
-	<body>
-	<center>
-		<p>
-			<h3>
-				<font color="blue">
-					Web-site, which can help you with lotteries, maybe...
-				</font>
-			</h3>
-		</p>
-		<p>
-			<b>
-				This RANDOM generates numbers from 1 to 100. Click "ROLL THIS!" to get your number!
-			</b>
-		</p>
-	</center>
-	<center>
-		<form action="index.php" method="POST">
-			<input type="submit" name="roll" value="ROLL THIS!">
-		</form>
-	<br />
-		<?php
-			if (isset ($_POST['roll'])) {
+		<head>
+			<title>
+			Random coin
+			</title>
+		</head>
+		<body>
+			<center>
+				<p>
+					<h3>
+						<font color="blue">
+							Web-site, which can help you with lotteries, maybe...
+						</font>
+					</h3>
+				</p>
+				<p>
+					<b>
+						This RANDOM-site generates the side of coin. Click "FLIP" to get the side of coin!
+					</b>
+				</p>
+			</center>
+			<center>
+				<form action="index.php" method="POST">
+					<input type="submit" name="flip" value="FLIP!">
+				</form>
+				<br />
+				<?php
+				if (isset ($_POST['flip'])) {
 				$rand = rand(1, 100);
-				echo $rand;
-			}
-		?>
-	</center>
-	</body>
+				if ($rand <= 50) {
+					echo 'HORSE';
+					} else {
+					echo 'FACE';
+							} 
+					}
+				?>
+			</center>
+		</body>
 	</html>
