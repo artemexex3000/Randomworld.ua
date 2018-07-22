58e6088acaa734f917a282a0660a867669762131
artemexex3000 <mrtrololoshka70@gmail.com>
2018-07-22 20:09:46 +0400

    Fixed bugs

--- index2.php
+++ index2.php
@@ -20,37 +20,31 @@
 						This RANDOM-site generates random number. Click "RANDOM" to get your number!
 					</b>
 				</p>
+				<p>
+					(0-999999)
+				</p>
 			</center>
 			<center>
 				<form action="index2.php" method="POST">
-					first number:<input type="number" name="fnumber">
+					first number:<input type="text" name="fnumber">
 					<br />
 					<br />
-					second number:<input type="number" name="snumber">
+					second number:<input type="text" name="snumber">
 					<br />
 					<br />
 					<input type="submit" name="random" value="RANDOM">
 				</form>
 				<br />
 				<?php
-				if ($_POST['fnumber'] <= 2147483647) {
-				if (isset ($_POST['random'])) {
-					$rand = rand($_POST['fnumber'], $_POST['snumber']);
-					echo $rand;
-				} } elseif ($_POST['fnumber'] >= -2147483648) {
-				if (isset ($_POST['random'])) {
-					$rand = rand($_POST['fnumber'], $_POST['snumber']);
-					echo $rand;
-				} } elseif ($_POST['snumber'] <= 2147483647) {
-				if (isset ($_POST['random'])) {
-					$rand = rand($_POST['fnumber'], $_POST['snumber']);
-					echo $rand;
-				} } elseif ($_POST['snumber'] >= -2147483648) {
-				if (isset ($_POST['random'])) {
+				if (is_numeric($_POST['fnumber']) and is_numeric($_POST['snumber'])) {
+				if ($_POST['fnumber'] >= 0 AND $_POST['fnumber'] <= 999999 AND $_POST['snumber'] >= 0 AND $_POST['snumber'] <= 999999) {
+					if (isset ($_POST['random'])) {
 					$rand = rand($_POST['fnumber'], $_POST['snumber']);
 					echo $rand;
 				} } else {
-					echo 'error';
+					echo 'Error';
+				} } else {
+					echo 'It must be a number!';
 				}
 				?>
 			</center>
